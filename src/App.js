1927ae7b3a9e2650121d7c8cbe02ec3caeaa900f
obnol <longboqiu@hotmail.com>
2022-04-30 09:48:55 +0200

    Added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './app/home/Home';
+import List from './app/list/List';
+import Tinder from './app/tinder/Tinder';
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route path='/list' element={<List />} />
+      <Route path='/tinder' element={<Tinder />} />
+    </Routes>
+  );
 }
 
 export default App;
